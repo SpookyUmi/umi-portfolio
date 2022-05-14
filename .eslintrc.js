923c09a3396cc0a96efedc22c9b085d50208427d
Chloé Danisevicius <chloe.danisevicius@gmail.com>
2022-05-14 19:37:23 +0200

    init

---
+++ .eslintrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    commonjs: true,
+    node: true,
+    es6: true,
+  },
+  extends: ['next/core-web-vitals', 'plugin:prettier/recommended'
+  ],
+  plugins: ['prettier'
+  ],
+}
